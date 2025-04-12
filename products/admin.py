ae5ef6ceb704f41889865254a2c7420e90f5ffc4
Erdey <erdeysyoum@gmail.com>
2025-04-12 16:27:08 +0300

    category and products is added to admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Product, Category, Inventory
+from .models import Product, Category, Inventory, ProductImage, ProductTag, ProductPriceHistory
+
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'current_price', 'status', 'created_at', 'updated_at']
+    list_filter = ['status', 'created_at', 'updated_at']
+    list_editable = ['current_price', 'status']
+    prepopulated_fields = {'slug': ('name', )}
+
 admin.site.register(Inventory)
+admin.site.register(ProductImage)
+admin.site.register(ProductTag)
+admin.site.register(ProductPriceHistory)
