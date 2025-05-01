a47293147812665df6b18b785cae2bd62600d820
Erdey <erdeysyoum@gmail.com>
2025-05-01 09:48:25 +0300

    checkout view added

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,6 @@ from .views import PlaceOrderView, OrderListView, CreateCheckoutSessionView, Str
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/place/', PlaceOrderView.as_view(), name='place-order'),
-    path('checkout/<int:order_id>/', CreateCheckoutSessionView, name='create-checkout-session'),
-    path('webhook/', StripeWebhookView, name='stripe-webhook'),
+    path('checkout/<int:order_id>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
 ]
