32762ef0e83bc7ebe3a5d4ba3c87048d016a4989
Erdey <erdeysyoum@gmail.com>
2025-05-01 10:15:06 +0300

    product serializer renamed

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import CartItem, Product
 
-class ProductSerializer(serializers.ModelSerializer):
+class CartProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'current_price']
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
+    product = CartProductSerializer(read_only=True)
     product_id = serializers.PrimaryKeyRelatedField(
         queryset=Product.objects.all(), source='products', write_only=True
     )
