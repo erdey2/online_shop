3d291142ef8f1d0394e20b215bd9224c3e6aa284
Erdey <erdeysyoum@gmail.com>
2025-05-01 09:45:41 +0300

    checkout view added

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PlaceOrderView, OrderListView, CreateCheckoutSessionView, stripe_webhook
+from .views import PlaceOrderView, OrderListView, CreateCheckoutSessionView, StripeWebhookView
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/place/', PlaceOrderView.as_view(), name='place-order'),
     path('checkout/<int:order_id>/', CreateCheckoutSessionView, name='create-checkout-session'),
-    path('webhook/', stripe_webhook, name='stripe-webhook'),
+    path('webhook/', StripeWebhookView, name='stripe-webhook'),
 ]
