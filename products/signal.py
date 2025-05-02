300806eb8c79ba12365a1bfaa4566fcb39fe3dfe
Erdey <erdeysyoum@gmail.com>
2025-05-02 11:33:55 +0300

    signal updated

--- products/signal.py
+++ products/signal.py
@@ -8,12 +8,11 @@ from .models import Inventory, Product, ProductPriceHistory
 def check_low_stock(sender, instance, **kwargs):
     if instance.stock <= settings.LOW_STOCK_THRESHOLD:
         # Send an email notification to admins and sellers
-        product_title = instance.product.title
-        subject = f"Low Stock Alert: {product_title}"
-        message = f"The stock for {product_title} is low. Only {instance.stock} units are left."
+        product_name = instance.product.name
+        subject = f"Low Stock Alert: {product_name}"
+        message = f"The stock for {product_name} is low. Only {instance.stock} units are left."
 
         # Example: Send an email to admins and sellers
-        # For simplicity, assuming admins and sellers have email addresses
         recipients = ['erdeysyoum@gamil.com', 'abelyitagesu@gmail.com']
 
         send_mail(subject, message, 'no-reply@example.com', recipients)
@@ -25,9 +24,12 @@ def update_product_availability(sender, instance, **kwargs):
 @receiver(pre_save, sender=Product)
 def log_price_change(sender, instance, **kwargs):
     if instance.pk:
-        previous = Product.objects.get(pk=instance.pk)
-        if previous.current_price != instance.current_price:
-            ProductPriceHistory.objects.create(
-                product=instance,
-                price=previous.current_price
-            )
+        try:
+            previous = Product.objects.get(pk=instance.pk)
+            if previous.current_price != instance.current_price:
+                print(f"Price changed from {previous.current_price} to {instance.current_price}")
+
+                ProductPriceHistory.objects.create(product=instance, price=previous.current_price)
+        except Product.DoesNotExist:
+            pass
+
