39e41c5314e0a4ee04b8f7a39a455abd6a0bb58b
Erdey <erdeysyoum@gmail.com>
2025-05-02 09:30:33 +0300

    name in category changed to title

--- products/models.py
+++ products/models.py
@@ -4,13 +4,13 @@ from uuid import uuid4
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=200, blank=True, null=True, db_index=True)
+    title = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('title',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -19,9 +19,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default="")
     # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
-    name = models.CharField(max_length=200, db_index=True, null=True)
+    name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     description = models.TextField(blank=True)
     current_price = models.DecimalField(max_digits=10, decimal_places=2)
